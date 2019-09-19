aab2653fe17957d5e5d7a8687a79e0b85b0ab15e
lndr27 <lndr27@hotmail.com>
2019-09-19 00:48:31 -0300

    adding notifications and fixing break time

--- src/angular-app/app.controller.js
+++ src/angular-app/app.controller.js
@@ -32,7 +32,7 @@ app.controller("MainController", function ($scope, $interval, AppService) {
      * Funcao acionada ao carregar a janela principal
      */
     $scope.init = function () {
-
+        $scope.isAlwaysOnTop = true;
         $scope.model = {
             horarios: []
         };
@@ -52,13 +52,14 @@ app.controller("MainController", function ($scope, $interval, AppService) {
 
         time.date = new Date();
         time.date.setHours(hours, min, 0, 0);
+        updateTimer();
     };
 
     $scope.addEntry = function () {
-        if ($scope.timesheet.length >= 6) {return;}
+        if ($scope.timesheet.length >= 6) { return; }
 
         var date = new Date();
-        $scope.timesheet.push({ 
+        $scope.timesheet.push({
             date: date,
             hours: `${_.padStart(date.getHours(), 2, '0')}:${_.padStart(date.getMinutes(), 2, '0')}`
         })
@@ -72,54 +73,70 @@ app.controller("MainController", function ($scope, $interval, AppService) {
 
         if ($scope.isRunning) {
             updateTimer();
-            $scope.timer = $interval(updateTimer, 1000);
+            $scope.timer = $interval(updateTimer, 60 * 1000);
         }
     };
 
     var updateTimer = () => {
         if (!$scope.isRunning) {
-            $scope.timer(); 
-            return; 
+            $scope.timer();
+            return;
         }
         if ($scope.timesheet.length === 0) { return; }
 
         var minutesWorked = getMinutesWorked($scope.timesheet);
         var mintuesBreak = getMinutesBreak($scope.timesheet);
-        
+
         var timeLeftBreak = 0;
 
         $scope.timeToLeave4hrs = new Date();
         $scope.timeToLeave4hrs.setMinutes($scope.timeToLeave4hrs.getMinutes() + (240 - minutesWorked));
+        if (minutesWorked === 240) {
+            _sendNotification('4 Horas', 'Já trabalhou 4 horas!');
+        }
         if (minutesWorked >= 240) {
             $scope.timeToLeave4hrs = 'errou';
         }
-        
+
         $scope.timeToLeave6hrs = new Date();
         timeLeftBreak = mintuesBreak - 15;
         timeLeftBreak = timeLeftBreak < 0 ? 0 : 15;
-        $scope.timeToLeave6hrs.setMinutes($scope.timeToLeave6hrs.getMinutes() + (375 - minutesWorked - timeLeftBreak));
-        if ((375 - minutesWorked - timeLeftBreak) <= 0) {
+        let diff = (375 - minutesWorked - timeLeftBreak);
+        $scope.timeToLeave6hrs.setMinutes($scope.timeToLeave6hrs.getMinutes() + diff);
+        if (diff === 0) {
+            _sendNotification('Já trabalhou 6 Horas', 'Já trabalhou 6 horas!');
+        }
+        if (diff <= 0) {
             $scope.timeToLeave6hrs = 'errou';
         }
 
         $scope.timeToLeave8hrs = new Date();
         timeLeftBreak = mintuesBreak - 60;
         timeLeftBreak = timeLeftBreak < 0 ? 0 : 60;
-        $scope.timeToLeave8hrs.setMinutes($scope.timeToLeave8hrs.getMinutes() + (540 - minutesWorked - timeLeftBreak));
-        if ((540 - minutesWorked - timeLeftBreak) <= 0) {
+        diff = (540 - minutesWorked - timeLeftBreak);
+        $scope.timeToLeave8hrs.setMinutes($scope.timeToLeave8hrs.getMinutes() + diff);
+        if (diff === 0) {
+            _sendNotification('Já trabalhou 8 Horas', 'Vai embora animal!');
+        }
+        if (diff <= 0) {
             $scope.timeToLeave8hrs = 'errou';
         }
-        
+
         $scope.timeToLeave10hrs = new Date();
-        $scope.timeToLeave10hrs.setMinutes($scope.timeToLeave10hrs.getMinutes() + (660 - minutesWorked - timeLeftBreak));
-        if ((660 - minutesWorked - timeLeftBreak) <= 0) {
+        diff = (660 - minutesWorked - timeLeftBreak);
+        $scope.timeToLeave10hrs.setMinutes($scope.timeToLeave10hrs.getMinutes() + diff);
+        if (diff === 0) {
+            _sendNotification('Já trabalhou 10 Horas', 'Ta fazendo que ainda aqui seu retardado?! Sai dai jumento!');
+        }
+        if (diff <= 0) {
             $scope.timeToLeave10hrs = 'errou';
         }
 
         var minutes = minutesWorked % 60;
         var hours = Math.floor(minutesWorked / 60);
         $scope.formatedTimeWorked = `${_.padStart(hours, 2, '0')}:${_.padStart(minutes, 2, '0')}`;
-        
+
+        if (mintuesBreak < 0) { mintuesBreak = 0; }
         minutes = mintuesBreak % 60;
         hours = Math.floor(mintuesBreak / 60);
         $scope.formatedTimeBreak = `${_.padStart(hours, 2, '0')}:${_.padStart(minutes, 2, '0')}`;
@@ -142,17 +159,19 @@ app.controller("MainController", function ($scope, $interval, AppService) {
     };
 
     const getMinutesBreak = (timesheet) => {
+        var agora = (new Date()).getTime();
         var minutes = 0;
-        if (timesheet.length >= 3) {
-            minutes += milisToMinutes(timesheet[2].date.getTime() - timesheet[1].date.getTime());
+        if (timesheet.length >= 2) {
+            minutes += milisToMinutes((_.get(timesheet, '[2].date.getTime()') || agora) - timesheet[1].date.getTime());
         }
-        if (timesheet.length >= 5) {
-            minutes += milisToMinutes(timesheet[4].date.getTime() - timesheet[3].date.getTime());
+        if (timesheet.length >= 4) {
+            minutes += milisToMinutes((_.get(timesheet, '[4].date.getTime()') || agora) - timesheet[3].date.getTime());
         }
         return minutes;
     };
 
     $scope.alwaysOnTop = function () {
+        $scope.isAlwaysOnTop = !$scope.isAlwaysOnTop;
         ipcRenderer.send('alwaysOnTop');
     };
 
@@ -186,5 +205,12 @@ app.controller("MainController", function ($scope, $interval, AppService) {
     $scope.maximizar = () => {
         browserWindow.isMaximized() ? browserWindow.restore() : browserWindow.maximize();
         $scope.isMaximizado = !$scope.isMaximizado;
+    };
+
+
+    const _sendNotification = function (titulo, msg) {
+        new Notification(titulo, {
+            body: msg
+        });
     };
 });
